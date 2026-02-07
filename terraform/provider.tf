a111c3f3ae622a1f2203351ea69e64ec7cff40fd
Kacper7011 <kacper.elblag@vp.pl>
2026-02-07 04:07:55 +0000

    add SSH configuration to proxmox provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 terraform {
     required_version = ">= 1.6.0, < 2.0.0"
-    
+
     required_providers {
       proxmox = {
         source = "bpg/proxmox"
@@ -13,4 +13,9 @@ provider "proxmox" {
     endpoint = var.proxmox_endpoint
     api_token = var.proxmox_api_token
     insecure = true
+
+    ssh {
+      username = "root"
+      private_key = file(var.proxmox_ssh_private_key_path)
+    }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,12 @@ variable "proxmox_api_token" {
     type = string
     sensitive = true
 }
+
+variable "ansible_ssh_public_key_path" {
+    type = string
+}
+
+variable "proxmox_ssh_private_key_path" {
+    type = string
+    description = "Path to SSH private key for Proxmox node access"
+}
